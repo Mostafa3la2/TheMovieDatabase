58415892a158db58d061ed2ee2f7801b93279554
Mostafa Alaa <mostafaassassin@gmail.com>
2024-07-20 20:05:37 +0300

    Initial Commit

---
+++ TheMovieDatabase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheMovieDatabase
+//
+//  Created by Mostafa Alaa on 20/07/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
