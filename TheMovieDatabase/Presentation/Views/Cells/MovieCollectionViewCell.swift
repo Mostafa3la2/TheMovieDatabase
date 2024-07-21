c55ed604b4b34b6b64e867e80a75adf86c424109
Mostafa Alaa <mostafaassassin@gmail.com>
2024-07-21 19:14:48 +0300

    added enhancements for the movie cell

--- TheMovieDatabase/Presentation/Views/Cells/MovieCollectionViewCell.swift
+++ TheMovieDatabase/Presentation/Views/Cells/MovieCollectionViewCell.swift
@@ -42,6 +42,11 @@ class MovieCollectionViewCell: UICollectionViewCell {
         addSubview(labelsStackView)
         labelsStackView.centerX(inView: self)
         labelsStackView.anchor(top: moviePosterImageView.bottomAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 8, paddingLeft: 4, paddingBottom: 8, paddingRight: 4)
+
+        movieTitleLabel.font = .boldSystemFont(ofSize: 18)
+        subtitleLabel.font = .systemFont(ofSize: 14, weight: .medium)
+        releaseDateLabel.font = .systemFont(ofSize: 12)
+
     }
 
     func configure(withMovie movie: MoviePresentationModel) {
@@ -49,6 +54,20 @@ class MovieCollectionViewCell: UICollectionViewCell {
         moviePosterImageView.kf.setImage(with: imageURL)
         movieTitleLabel.text = movie.title
         subtitleLabel.text = movie.voteAverage != nil ? String(format: "%.2f", movie.voteAverage!) : "NA"
+        if let averageRating = movie.voteAverage {
+            if (0..<6).contains(averageRating) {
+                subtitleLabel.textColor = .red
+            }
+            else if (6..<7.5).contains(averageRating) {
+                subtitleLabel.textColor = .orange
+            }
+            else if (7.5...10).contains(averageRating) {
+                subtitleLabel.textColor = .systemGreen
+            }
+            else {
+                subtitleLabel.textColor = .black
+            }
+        }
         releaseDateLabel.text = movie.releaseDate
     }
 }
